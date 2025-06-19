c393c2c33fae573df4547ef049ddf652f80f7f9e
amathivanan <amathivanan@griddynamics.com>
2025-06-19 17:36:35 +0530

    ADD SIDEBAR TOGGLE FUNCTIONALITY

--- front-end/test-project/src/components/Sidebar.jsx
+++ front-end/test-project/src/components/Sidebar.jsx
@@ -1,47 +1,74 @@
 import gridLogo from "../assets/gd.png";
+import gridLogoSmall from "../assets/GridLogoSmall.png";
 import { Link } from "react-router-dom";
-import { useCallback } from "react";
+import { useCallback, useState, useEffect } from "react";
 
-export default function Sidebar() {
-  const handleSidebarClick = useCallback((e) => {
-    const target = e.target.closest("a");
-    if (!target) return;
-    console.log(target);
+export const Sidebar = function () {
+  const [isOpen, setIsOpen] = useState(true);
+  const [showText, setShowText] = useState(false);
 
-    const links = e.currentTarget.querySelectorAll("a");
+  useEffect(() => {
+    let timeout;
+    if (isOpen) {
+      timeout = setTimeout(() => {
+        setShowText(true);
+      }, 200); // 200ms delay
+    } else {
+      setShowText(false); // hide immediately when closing
+    }
 
-    links.forEach((link) => {
-      link.classList.remove(
-        "bg-gradient-to-r",
-        "from-violet-100",
-        "to-pink-100"
-      );
+    return () => clearTimeout(timeout);
+  }, [isOpen]);
 
-      if (link === target) {
-        link.classList.add(
-          "bg-gradient-to-r",
-          "from-violet-100",
-          "to-pink-100"
-        );
-      }
-    });
+  const handleToggle = useCallback(() => {
+    setIsOpen((prev) => !prev);
   }, []);
 
   return (
     <div
-      className="bg-[#ffffff] min-w-[250px] min-h-dvh h-max px-5 pt-6 pb-10 flex flex-col"
-      onClick={handleSidebarClick}
+      className={`bg-[#ffffff] min-h-dvh h-max px-4 pt-6 pb-10 flex flex-col transition-all duration-200 justify-center ${
+        isOpen ? "w-64 min-w-[250px]" : "w-20 min-w-[72px]"
+      }`}
     >
-      <div className="flex items-center gap-2 ">
-        <img src={gridLogo} alt="" className="h-8" />
+      <div
+        className={`flex gap-2 transition-all duration-200 ease-in-out items-center text-violet-800 font-semibold text-lg ${
+          !isOpen ? "justify-center" : "px-3"
+        } `}
+      >
+        <img
+          src={gridLogoSmall}
+          alt="logo"
+          className={`w-8 object-contain h-8`}
+        />
+
+        <span className="transition-all duration-200 ease-in-out delay-200">
+          {showText ? "Grid Dynamics" : ""}
+        </span>
       </div>
 
-      <nav className="flex flex-col gap-2 mt-14">
-        {/* <Link
-          to="/"
-          className="flex gap-3 items-center bg-gradient-to-r from-violet-100 to-pink-100 text-violet-800  hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100 rounded-md  p-3 
-          transition duration-200 ease-in-out"
-        >
+      <button
+        onClick={handleToggle}
+        className="flex items-center gap-2 p-3 rounded  transition  text-violet-800 hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100 mt-8 h-12 justify-between"
+        aria-label={isOpen ? "Collapse sidebar" : "Expand sidebar"}
+      >
+        {showText && <span className="text-2xl ">Hi John</span>}
+
+        {isOpen ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
+            />
+          </svg>
+        ) : (
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -53,15 +80,16 @@ export default function Sidebar() {
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"
             />
           </svg>
-          <span>Dashboard</span>
-        </Link> */}
+        )}
+      </button>
 
+      <nav className="flex flex-col gap-2 mt-4">
         <Link
           to="/project"
-          className="flex gap-3 items-center  hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100   text-violet-800  p-3 rounded-md transition duration-200 ease-in-out"
+          className="flex gap-3 items-center hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100 text-violet-800 p-3 rounded-md transition duration-200 ease-in-out"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -77,13 +105,11 @@ export default function Sidebar() {
               d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
             />
           </svg>
-
-          <span>Projects</span>
+          {isOpen && <span>Projects</span>}
         </Link>
-
         <Link
           to="/"
-          className="flex gap-3 items-center  hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100   text-violet-800  p-3 rounded-md transition duration-200 ease-in-out"
+          className="flex gap-3 items-center hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100 text-violet-800 p-3 rounded-md transition duration-200 ease-in-out"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -99,13 +125,11 @@ export default function Sidebar() {
               d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
             />
           </svg>
-
-          <span>Test-Case</span>
+          {isOpen && <span>Test-Case</span>}
         </Link>
-
         <Link
           to="/"
-          className="flex gap-3 items-center  hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100   text-violet-800  p-3 rounded-md transition duration-200"
+          className="flex gap-3 items-center hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100 text-violet-800 p-3 rounded-md transition duration-200"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -121,13 +145,11 @@ export default function Sidebar() {
               d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z"
             />
           </svg>
-
-          <span>Manage</span>
+          {isOpen && <span>Manage</span>}
         </Link>
-
         <Link
           to="/project/reports"
-          className="flex gap-3 items-center  hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100   text-violet-800  p-3 rounded-md transition duration-200"
+          className="flex gap-3 items-center hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100 text-violet-800 p-3 rounded-md transition duration-200"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -143,8 +165,7 @@ export default function Sidebar() {
               d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
             />
           </svg>
-
-          <span>Reports</span>
+          {isOpen && <span>Reports</span>}
         </Link>
       </nav>
 
@@ -153,7 +174,7 @@ export default function Sidebar() {
       <nav className="flex flex-col gap-0 mt-4">
         <Link
           to="/"
-          className="flex gap-3 items-center  hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100   text-violet-800  p-3 rounded-md transition duration-200"
+          className="flex gap-3 items-center hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100 text-violet-800 p-3 rounded-md transition duration-200"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -174,13 +195,11 @@ export default function Sidebar() {
               d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
             />
           </svg>
-
-          <span>Settings</span>
+          {isOpen && <span>Settings</span>}
         </Link>
-
         <Link
           to="/"
-          className="flex gap-3 items-center  hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100   text-violet-800  p-3 rounded-md transition duration-200"
+          className="flex gap-3 items-center hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-100 text-violet-800 p-3 rounded-md transition duration-200"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -196,10 +215,11 @@ export default function Sidebar() {
               d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
             />
           </svg>
-
-          <span>Help & Support</span>
+          {isOpen && <span>Settings</span>}
         </Link>
       </nav>
     </div>
   );
-}
+};
+
+export default Sidebar;
